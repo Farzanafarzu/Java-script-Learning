e3c3a09535b756610f43b4268251561e3c46bd5a
Farzanafarzu <farzu9034@gmail.com>
2024-10-20 14:14:35 +0000

    conversion and operation discuss

--- 01_basics.js/conversionOperation.js
+++ 01_basics.js/conversionOperation.js
@@ -12,10 +12,12 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "654"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ //console.log(booleanIsLoggedIn);
+// console.log(typeof (booleanIsLoggedIn));
+ 
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,7 +26,7 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
+ //console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
@@ -53,8 +55,8 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+ //console.log(+true);
+ //console.log(+"");
 
 let num1, num2, num3
 
